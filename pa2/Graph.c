5701e6fc80d59ca057d000b0b7afd8c798c6e2e7
Tim Kraemer <tikraeme@ucsc.edu>
2022-12-19 14:48:46 -0800

    fixed Graph constructor, and memory leaks

--- pa2/Graph.c
+++ pa2/Graph.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "Graph.h"
 #include "List.h"
@@ -8,6 +9,7 @@ typedef struct GraphObj {
     int vertices;
     int edges;
     int vertex_source;
+    bool BFS_run;
     List* neighbors;
     int* color;
     int* parent;
@@ -17,36 +19,81 @@ typedef struct GraphObj {
 Graph newGraph(int n) {
     Graph g = malloc(sizeof(GraphObj));
     g->vertices = n;
-    g->edges = 0;
-    g->vertex_source = NIL;
-    g->color = (int *) calloc(n, sizeof(int));
-    g->parent = (int *) calloc(n, sizeof(int));
-    g->distance = (int *) calloc(n, sizeof(int));
-    g->neighbors = (List*) calloc(n, sizeof(List));
+    g->edges = NULL;
+    g->vertex_source = NULL;
+    g->BFS_run = false;
+    g->color = (int *) calloc(n+1, sizeof(int));
+    g->parent = (int *) calloc(n+1, sizeof(int));
+    g->distance = (int *) calloc(n+1, sizeof(int));
+    g->neighbors = (List*) calloc(n+1, sizeof(List));
     return(g);
 }
 
 void freeGraph(Graph* pG) {
     if(pG != NULL && *pG != NULL) {
-        if(&((*pG)->color) != NULL && ((*pG)->color) != NULL) {
-            free(&((*pG)->color));
+        if((*pG)->color != NULL) {
+            free((*pG)->color);
             (*pG)->color = NULL;
         }
-        if(&((*pG)->parent) != NULL && ((*pG)->parent) != NULL) {
-            free(&((*pG)->parent));
+        if((*pG)->parent != NULL) {
+            free((*pG)->parent);
             (*pG)->parent = NULL;
         }
-        if(&((*pG)->distance) != NULL && ((*pG)->distance) != NULL) {
-            free(&((*pG)->distance));
+        if((*pG)->distance != NULL) {
+            free((*pG)->distance);
             (*pG)->distance = NULL;
         }
-        if(&((*pG)->neighbors) != NULL && ((*pG)->neighbors) != NULL) {
+        if((*pG)->neighbors != NULL) {
             for(int i = 0; i < (*pG)->vertices; i++) {
                 freeList((*pG)->neighbors);
             }
+            free((*pG)->neighbors);
             (*pG)->neighbors = NULL;
         }
         free(*pG);
         *pG = NULL;
     }
+}
+
+int getOrder(Graph G) {
+    return G->vertices;
+}
+
+int getSize(Graph G) {
+    return G->edges;
+}
+
+int getSource(Graph G) {
+    if(!G->BFS_run) {
+        return NIL;
+    }
+    else {
+        return G->vertex_source;
+    }
+}
+
+int getParent(Graph G, int u) {
+    if(!G->BFS_run) {
+        return NIL;
+    }
+    else {
+        return (G->parent)[u];
+    }
+}
+
+getDist(Graph G, int u) {
+    if(!G->BFS_run) {
+        return INF;
+    }
+    else {
+        return (G->distance)[u];
+    }
+}
+
+void getPath(List L, Graph G, int u) {
+
+}
+
+void makeNull(Graph G) {
+    
 }
