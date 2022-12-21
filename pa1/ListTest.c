6e1641ce3ccc91cd474afd22faa93ba12be84533
Tim Kraemer <tikraeme@ucsc.edu>
2022-12-21 01:25:27 -0800

    actually finished pa1, fixed some issues

--- pa1/ListTest.c
+++ pa1/ListTest.c
@@ -5,13 +5,5 @@
 #include "List.h"
 
 int main() {
-    List test1;
-
-    test1 = newList();
-
-    clear(test1);
-
-    printf("List Length: %d, List index = %d\n", length(test1), index(test1));
-
-    freeList(&test1);
+    
 }
