11d7a0536905f59feeddf55f78d2d178ce18a4e4
Tim Kraemer <tikraeme@ucsc.edu>
2022-12-24 02:43:02 -0800

    finished FindPath, it works with 2 example inputs and has no memory leaks

--- pa2/FindPath.c
+++ pa2/FindPath.c
@@ -13,6 +13,9 @@ int main(int arc, char* argv[]) {
     }
 
     FILE* in_fp = fopen(argv[1], "r");
+    FILE* out_fp = fopen(argv[2], "w");
+
+    
 
     if(in_fp == NULL) {
         fprintf(stderr, "IO Error: Could not open file %s\n", argv[1]);
@@ -28,23 +31,52 @@ int main(int arc, char* argv[]) {
 
     int vert1;
     int vert2;
-    for(int i = 1; i <= num_vert; i++) {
-        if(fscanf(in_fp, "%d %d", &vert1, &vert2) == 0) {
-            fprintf(stderr, "Error: file format incorrect\n");
-            return -1;
+    
+    while(1) {
+        fscanf(in_fp, "%d %d", &vert1, &vert2);
+        if(vert1 == 0 && vert2 == 0) {
+            break;
         }
         addEdge(graph1, vert1, vert2);
     }
 
-    fscanf(in_fp, "%d %d", &vert1, &vert2);
-    if(vert1 != 0 || vert2 != 0) {
-        fprintf(stderr, "Error: file format incorrect\n");
-        return -1;
+    printGraph(out_fp, graph1);
+
+    fprintf(out_fp, "\n");
+
+    int source;
+    int dest;
+
+    while(1) {
+        fscanf(in_fp, "%d %d", &source, &dest);
+        if(source == 0 && dest == 0) {
+            break;
+        }
+        BFS(graph1, source);
+        //printing the distance of the two vertices
+        if(getDist(graph1, dest) == INF) {
+            fprintf(out_fp, "The distance from %d to %d is infinity\n", source, dest);
+        }
+        else {
+            fprintf(out_fp, "The distance from %d to %d is %d\n", source, dest, getDist(graph1, dest));
+
+        }
+        //printing the path of the two vertices
+        if(getDist(graph1, dest) == INF) {
+            fprintf(out_fp, "No %d-%d path exists\n", source, dest);
+        }
+        else {
+            fprintf(out_fp, "A shortest %d-%d path is: ", source, dest);
+            List temp = newList();
+            getPath(temp, graph1, dest);
+            printList(out_fp, temp);
+            freeList(&temp);
+            fprintf(out_fp, "\n");
+        }
     }
 
-    printGraph(stdout, graph1);
-
     fclose(in_fp);
+    fclose(out_fp);
 
     freeGraph(&graph1);
 }
